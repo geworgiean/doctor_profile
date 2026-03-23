3c0ce5e67a1a010be010d693595d7cc746edf3b5
Arman Gevorgyan <geworgiean@gmail.com>
2026-03-23 22:38:59 +0400

    change function

--- src/app/actions/profile.ts
+++ src/app/actions/profile.ts
@@ -2,17 +2,22 @@
 import { prisma as db } from '@/lib/db';
 
 export async function saveDoctorProfile(data: any) {
+  if (!data.userId) {
+    throw new Error("User ID-ն գտնված չէ: Խնդրում ենք նորից մուտք գործել:");
+  }
+
   try {
-    const existingProfile = await db.profile.findFirst({
-      where: { userId: data.userId },
-    });
-
-    if (existingProfile) {
-      throw new Error("Դուք արդեն ունեք ստեղծված պրոֆիլ:");
-    }
-
-    return await db.profile.create({
-      data: {
+    return await db.profile.upsert({
+      where: { 
+        userId: data.userId 
+      },
+      update: {
+        firstName: data.firstName,
+        lastName: data.lastName,
+        specialty: data.specialty,
+        documentUrl: data.documentUrl,
+      },
+      create: {
         firstName: data.firstName,
         lastName: data.lastName,
         specialty: data.specialty,
