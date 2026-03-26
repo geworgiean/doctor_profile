2991f707efdc86f42b0d32112fd73bb10085907b
Arman Gevorgyan <geworgiean@gmail.com>
2026-03-26 20:21:11 +0400

    added more fucion

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -43,14 +43,13 @@ const session = await getServerSession(authOptions);
     return (
       <main className="min-h-screen bg-slate-50 flex flex-col items-center">
         <nav className="w-full bg-white border-b border-slate-200 py-4 px-6 md:px-10 flex justify-between items-center shadow-sm sticky top-0 z-10">
-          <h1 className="text-2xl font-bold text-blue-600 tracking-tight italic">MediProfile</h1>
           <Link 
             href="/" 
             className="flex items-center gap-2 text-sm text-slate-600 hover:text-blue-600 transition font-medium"
           >
             ← Հետ
           </Link>
-
+          <h1 className="text-2xl font-bold text-blue-600 tracking-tight italic">MediProfile</h1>
           <div className="flex items-center gap-4 border border-slate-100 bg-slate-50 px-4 py-2 rounded-full">
             <p className="text-sm text-slate-700">Բարև, <span className="font-semibold">{session.user.name}</span></p>
           </div>
